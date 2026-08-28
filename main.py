6eab316cf109e31871cdffb0d40f26805d70a947
sakshimanwar1206 <sakshi.manwar1206@gmail.com>
2026-08-28 21:35:01 +0530

    C: added messages list for multiple messages and added optional argument verbose for more info

--- main.py
+++ main.py
@@ -3,41 +3,46 @@ import argparse
 from dotenv import load_dotenv
 from openai import OpenAI
 
-load_dotenv()
-api_key = os.environ.get("OPENROUTER_API_KEY")
+def main():
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
 
-if api_key == None:
-        raise RuntimeError("API Key not found")
+    if api_key == None:
+            raise RuntimeError("API Key not found")
 
-client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=api_key,
-)
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
 
-parser = argparse.ArgumentParser(description="Chatbot")
-parser.add_argument("user_prompt", type=str, help="User prompt")
-args = parser.parse_args()
-# Now we can access `args.user_prompt`
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
 
-response = client.chat.completions.create(
-    model="openrouter/free",
-    messages=[
-        {
-            "role": "user",
-            "content": args.user_prompt,
-        }
-    ],
-)
+    messages = [
+        {"role": "user", "content": args.user_prompt},
+    ]
 
-if response.usage == None:
-     raise RuntimeError("failed API request")
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=messages,
+    )
 
-print(f"Prompt tokens: {response.usage.prompt_tokens} \n" 
-      f"Response tokens: {response.usage.completion_tokens} \n"
-      f"Response: \n{response.choices[0].message.content}")
+    if response.usage == None:
+        raise RuntimeError("failed API request")
 
-#print(response.choices[0].message.content)
+    if args.verbose == True:
+        print(f"User prompt: {args.user_prompt} \n"
+            f"Prompt tokens: {response.usage.prompt_tokens} \n" 
+        f"Response tokens: {response.usage.completion_tokens} \n"
+        f"Response: \n{response.choices[0].message.content}")
+    else:
+        print(f"Response: \n{response.choices[0].message.content}")
+
+
     
 
-#if __name__ == "__main__":
-    #main()
+if __name__ == "__main__":
+    main()
