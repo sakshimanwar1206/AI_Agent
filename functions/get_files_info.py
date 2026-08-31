09a7dfbffec0e66999136b405ffb2f4d23789646
sakshimanwar1206 <sakshi.manwar1206@gmail.com>
2026-08-31 21:20:01 +0530

    F: created a function to list the contents of a directory

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -6,13 +6,22 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
         valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
 
+        print(f"Result for '{directory}' directory:")
         if valid_target_dir == False:
-            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+            return f'   Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-        if valid_target_dir == True and not os.path.isdir(directory):
-            return f'Error: "{directory}" is not a directory'
+        if valid_target_dir == True and not os.path.isdir(target_dir):
+            return f'   Error: "{directory}" is not a directory'
 
-        return f'Success: "{directory}" is within the working directory'
+        files_info: list[str] = []
+        for filename in os.listdir(target_dir):
+            filepath = os.path.join(target_dir, filename)
+            is_dir = os.path.isdir(filepath)
+            file_size = os.path.getsize(filepath)
+            files_info.append(
+                f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}"
+            )
+        return "\n".join(files_info)
         
     except Exception as e:
         print(f"Error: {e}")
--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,6 +1,6 @@
 from functions.get_files_info import get_files_info
 
-get_files_info("calculator", ".")
-get_files_info("calculator", "/bin")
-get_files_info("calculator", "../")
-get_files_info("calculator", "main.py")
+print(get_files_info("calculator", "."))
+print(get_files_info("calculator", "pkg"))
+print(get_files_info("calculator", "/bin"))
+print(get_files_info("calculator", "../"))
