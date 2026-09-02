85a0d67a3ccf44a1374f89a116ffb162ea6a4cdc
sakshimanwar1206 <sakshi.manwar1206@gmail.com>
2026-09-02 21:20:51 +0530

    H: added a function to write and overwrite files, within working directory provided to Agent

---
+++ functions/write_file.py
@@ -0,0 +1,18 @@
+import os
+
+def write_file(working_directory: str, file_path: str, content: str) -> str:
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_file_path = os.path.normpath(os.path.join(working_dir_abs, file_path))
+        if os.path.commonpath([working_dir_abs, target_file_path]) != working_dir_abs:
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        if os.path.isdir(target_file_path):
+            return f'Error: Cannot write to "{file_path}" as it is a directory'
+        with open(target_file_path,"w") as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+
+    except Exception as e:
+        return f"ERROR: {e}"
+    
+    
